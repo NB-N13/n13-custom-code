9f192887860998ad075ea9eba1155bd7207ebfbd
NB-N13 <nb@n13.media>
2022-12-16 16:58:58 +0100

    Create language-switch_01.js

---
+++ src/language-switch_01.js
@@ -0,0 +1,24 @@
+var userLang = navigator.language || navigator.userLanguage; 
+console.log("The language is: " + userLang);
+if ((Cookies.get("language")) === "en" || (Cookies.get("language")) === "de" || (Cookies.get("language")) === "fr"){
+	if ((Cookies.get("language")) === "de"){
+  window.location.href = "/de/home" 
+  }
+  else{
+  if ((Cookies.get("language")) === "fr"){
+  window.location.href = "/fr/home" 
+  }
+  }
+}
+else {
+		if (userLang.toLowerCase().includes('de')) {
+    	  Cookies.set('language', "de");
+   	   window.location.href = "/de/home"  
+      } 
+    else{
+    if (userLang.toLowerCase().includes('fr')) {
+      Cookies.set('language', "fr");
+      window.location.href = "/fr/home"      
+			}
+    }
+}
